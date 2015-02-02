0f6aa4aea63463a316fe76c5f7be8f96d6f22e88
zhan shun <garretthh07@gmail.com>
2015-02-02 19:05:50 +0800

    add dictionary sample

--- A_Byte_of_Python/using_dict.py
+++ A_Byte_of_Python/using_dict.py
@@ -8,7 +8,6 @@ ab = { 'Swaroop': 'swaroop@byteofpython.info',
         'Matsumoto': 'matz@ruby-lang.org',
         'Spammer': 'spammer@hotmail.com'
         }
-
 print "Swaroop's address is %s" % ab['Swaroop']
 
 # Adding a key/value pair
@@ -18,3 +17,9 @@ ab['Guido'] = 'guido@python.org'
 del ab['Spammer']
 
 print '\nThere are %d contacts in the address-book\n' % len(ab)
+
+for name, address in ab.items():
+    print 'Contact %s at %s' % (name, address)
+
+if 'Guido' in ab: # OR ab.has_key('Guido')
+    print "\nGuido's address is %s" % ab['Guido']
