f55d49be4e655bd895d71324e5d6d82331558778
zhan shun <garretthh07@gmail.com>
2015-02-09 15:05:49 +0800

    add rename.vim and Vim/vimbook.vim

---
+++ Vim/rename.cpp
@@ -0,0 +1,20 @@
+John Doe
+Peter Smith  
+
+    if (strstr(input, "aap") == ) /* vim:set shiftwidth=2: */
+    if (strstr(input, "aap") == ) /* vim:set shiftwidth=2: */
+    if (strstr(input, "aap") == ) /* vim:set shiftwidth=2: */
+    if (strstr(input, "aap") == ) /* vim:set shiftwidth=2: */
+    if (strstr(input, "aap") == ) /* vim:set shiftwidth=2: */
+    if (strstr(input, "aap") == ) /* vim:set shiftwidth=2: */
+    if (strstr(input, "aap") == )
+    if (strstr(input, "aap") == )
+    if (strstr(input, "aap") == )
+    if (strstr(input, "aap") == )
+    if (strstr(input, "aap") == )
+    if (strstr(input, "aap") == )
+    if (strstr(input, "aap") == )
+    if (strstr(input, "aap") == )
+    if (strstr(input, "aap") == )
+    if (strstr(input, "aap") == )
+    if (strstr(input, "aap") == )
