25d1ac93c2f27bb043c5b50e4a240e5d672859cc
zhan shun <garretthh07@gmail.com>
2015-02-11 09:46:06 +0800

    add sample for vim.pdf

--- Vim/a.py
+++ Vim/a.py
@@ -1,5 +1,49 @@
 abcdefghijklmnopqrstuvwxyz
-four zhangshun Hi, everyone
-impetus
-four zhang Ja Ha OH
-int
+zyxwvutsrqponmlkjihgfedcba
+abcdefghijklmnopqrstuvwxyz
+zyxwvutsrqponmlkjihgfedcba
+12345678900987654321
+-_=+/?\|~`()<>!@^&*#$%
+*_-++qwertyuiop=+asdfghjkl;
+/.,mnbvcxz
+UIOPYJLKN>MBVEWDFAZX
+1234567890--=+[]{}ppp[]abcd[]
+qwertp[]{}6^tt5%
+
+
+    ftp://ftp.vim.org/pub/vim/README
+
+https://github.com/Garretthh07/Python/blob/master/Dive_into_python/Chapter_2/main.py
+
+(example)
+
+total = ch_array[0] +
+ch_array[1] + ch_array[2]
+
+a.py
+
+b_array[i]->s_next =
+a_array[i]->s_next
+b_array[i]->s_next =
+a_array[i]->s
+
+abcdefghijklmnopqrstuvwxyz
+zyxwvutsrqponmlkjihgfedcba
+120345=+-_67890p]}[{=+-_-_
+    =+-_[{}] advertisement
+
+12345678900123456789012345678901234567890
+I taught for a while. One
+time, I was stopped by the
+Fort Worth police, because my
+homework was too hard. True
+story.
+
+$VIMRUNTIME/macros/justify.vim
+
+nice table  test 3    test 1  test 2  test 3
+input A     0.693     0.534           0.693
+input B    0.111     0.913           0.111
+
+inp 0.693   0.534   0.693
+
