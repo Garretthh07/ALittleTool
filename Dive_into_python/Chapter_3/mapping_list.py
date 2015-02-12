41241eaa9bbfb698bce6e04144ca7b40ae05d8cc
zhan shun <garretthh07@gmail.com>
2015-02-12 09:47:44 +0800

    add sample for tuples.py

---
+++ Dive_into_python/Chapter_3/mapping_list.py
@@ -0,0 +1,5 @@
+#!usr/bin/python
+# Filename: mapping_list.py
+
+li = [1, 9, 8, 4]
+
---
+++ Dive_into_python/Chapter_3/tuples.py
@@ -0,0 +1,11 @@
+#!usr/bin/python
+# Filename: tuples.py
+
+t = ("a", "b", "mpilgrim", "z", "example")
+print t
+
+print t[0]
+print t[-1]
+
+# when slice a list, get a new list
+print t[1:3]
