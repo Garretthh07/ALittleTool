0bf604aad53628e54b4df7b95aecb2ad7fef5467
张顺 <zhangshun@zhangshundeMac-mini.local>
2015-02-20 20:13:42 +0800

    update to my mac

--- A_Byte_of_Python/if.py
+++ A_Byte_of_Python/if.py
@@ -4,8 +4,7 @@
 number = 23
 guess = int(raw_input('Enter an integer : '))
 
-if guess == number:
-    print 'Congratulation, you guessed it.' # New block starts here
+if guess == number: print 'Congratulation, you guessed it.' # New block starts here
     print "(but you do not win any prizes!" # New block ends here
 elif guess < number:
     print 'No, it is a  little higher than that' #Another block
