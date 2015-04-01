5abd4d8d8a6af1994195de746e0299a008f4088b
zhan shun <garretthh07@gmail.com>
2015-04-01 17:43:21 +0800

    sample of standard

---
+++ pythonTip/Chapter_10/10_1.py
@@ -0,0 +1,5 @@
+#!usr/bin/env python
+
+import shutil
+shutil.copyfile('../README.md', 'README.md')
+shutil.move('../Chapter_9', 'installdir')
---
+++ pythonTip/Chapter_10/README.md
@@ -0,0 +1,1 @@
+> Python Tips: [@标准库](http://www.pythontip.com/study/books/pythontutorial3-master)
